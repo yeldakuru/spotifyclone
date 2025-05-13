b91d8b8f4426ca52de6225a823a2582d875dfd38
Yelda <yeldakuru99@outlook.com>
2025-05-13 17:49:40 +0300

    created models

---
+++ backend/src/models/song.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const songSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    artist: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+    },
+    audioUrl: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    albumId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Album",
+        required: false,
+    },
+}, { timestamps: true }); //createdAt and updatedAt
+
+export const Song = mongoose.model('Song', songSchema);
